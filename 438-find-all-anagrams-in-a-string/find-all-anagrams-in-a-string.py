a6faaa9aa51839e1f1f3746cc0f663fcac9c0104
Avnish Mishra <0131cs221049@jnctbhopal.ac.in>
2026-09-13 21:22:08 +0530

    Time: 31 ms (98.36%) | Memory: 13.5 MB (49.83%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
@@ -1,40 +1,41 @@
 class Solution:
     def findAnagrams(self, s, p):
+        if len(p)>len(s):
+            return[]
 
-        if len(p) > len(s):
-            return []
+        count_p=[0]*26
+        count_s=[0]*26
 
-        count_p = [0] * 26
-        count_s = [0] * 26
-
-        # Frequency of p
         for ch in p:
-            count_p[ord(ch) - ord('a')] += 1
-
-        # First window
+            count_p[ord(ch)-ord('a')]+=1
         for i in range(len(p)):
-            count_s[ord(s[i]) - ord('a')] += 1
-
+            count_s[ord(s[i])-ord('a')]+=1
         result = []
+        left = 0
 
         if count_p == count_s:
             result.append(0)
 
-        # Sliding window
         for right in range(len(p), len(s)):
 
-            # Add new character
             count_s[ord(s[right]) - ord('a')] += 1
-
-            # Remove old character
-            left = right - len(p)
             count_s[ord(s[left]) - ord('a')] -= 1
 
-            # Check anagram
-            if count_p == count_s:
-                result.append(left + 1)
+            left += 1
 
-        return result
+            if count_p == count_s:
+                result.append(left)
+
+        return result       
+
+                
+
+            
+
+
+   
+
+        
         """
         :type s: str
         :type p: str
